a555f0053ee3a0a63ba3b7337d585a2892576965
miguel-l-b <miguellopesbraido@gmail.com>
2023-08-14 23:31:14 -0300

    ref: removing `;`

--- src/main/socket.ts
+++ src/main/socket.ts
@@ -1,34 +1,34 @@
 import { Socket, Server as SocketServer } from "$socket"
 
 export default class Socket_IO {
-	private static instance: Socket_IO;
-	private readonly host: string = Deno.env.get("HOST") || "localhost";
-	public readonly port: number = Deno.env.get("PORT_SOCKET") ? parseInt(Deno.env.get("PORT_SOCKET")!) : 3000;
-	public readonly server = new SocketServer(this.port);
-	private isListening = false;
-	private middlewares: {[key: string]: (data: any, socket: Socket, next: () => void) => void} = {};
+	private static instance: Socket_IO
+	private readonly host: string = Deno.env.get("HOST") || "localhost"
+	public readonly port: number = Deno.env.get("PORT_SOCKET") ? parseInt(Deno.env.get("PORT_SOCKET")!) : 3000
+	public readonly server = new SocketServer(this.port)
+	private isListening = false
+	private middlewares: {[key: string]: (data: any, socket: Socket, next: () => void) => void} = {}
 	private connections: {
-		[key: string]: (data: any, socket: Socket) => void;
+		[key: string]: (data: any, socket: Socket) => void
 	} = {};
 	constructor() {
 		if (Socket_IO.instance)
-			return Socket_IO.instance;
-		Socket_IO.instance = this;
+			return Socket_IO.instance
+		Socket_IO.instance = this
 	}
 
 	public listen() {
 		if(this.isListening)
-			throw new Error("Socket already listening");
+			throw new Error("Socket already listening")
 
-		this.isListening = true;
-		console.log(`Socket listening on port ${this.port}`);
+		this.isListening = true
+		console.log(`Socket listening on port ${this.port}`)
 		this.server.on("connection", (socket) => {
 			this.handleMiddleware("connection", socket.data, socket, () => {
-				this.connections["connection"]?.(socket.data, socket);
-			});
-			this.handleConnection(socket);
+				this.connections["connection"]?.(socket.data, socket)
+			})
+			this.handleConnection(socket)
 			this.handleMiddleware("disconnect", socket.data, socket, () => {
-				this.connections["disconnect"]?.(socket.data, socket);
+				this.connections["disconnect"]?.(socket.data, socket)
 			})
 		})
 	}
@@ -46,34 +46,34 @@ export default class Socket_IO {
 	}
 
 	private handleMiddleware(key: string, data: any, socket: Socket, next: () => void) {
-		this.middlewares[key]? this.middlewares[key](data, socket, next) : next();
+		this.middlewares[key]? this.middlewares[key](data, socket, next) : next()
 	}
 
 	public use(key: string, callback: (data: any, socket: Socket, next: () => void) => void) {
 		if(this.middlewares[key])
-			throw new Error("Key already exists");
+			throw new Error("Key already exists")
 
-		this.middlewares[key] = callback;
+		this.middlewares[key] = callback
 	}
 
 	public add(key: string, callback: (data: any, socket: Socket) => void) {
 		if(this.connections[key])
-			throw new Error("Key already exists");
+			throw new Error("Key already exists")
 
-		this.connections[key] = callback;
+		this.connections[key] = callback
 	}
 
 	public remove(key: string) {
 		if(!this.connections[key])
-			throw new Error("Key not found");
+			throw new Error("Key not found")
 
-		delete this.connections[key];
+		delete this.connections[key]
 	}
 
 	public get(key: string) {
 		if(!this.connections[key])
-			throw new Error("Key not found");
+			throw new Error("Key not found")
 
-		return this.connections[key];
+		return this.connections[key]
 	}
 }
