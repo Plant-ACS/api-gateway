b3e89235d9af39999f8813420ec952b41f16ba01
Bruno <brunoconcli@gmail.com>
2023-08-21 20:55:57 -0300

    ref: Change interface to type

--- src/core/use-cases/device/update.ts
+++ src/core/use-cases/device/update.ts
@@ -1,11 +1,9 @@
 import { Device } from "@core/entities/device.ts"
 
-export interface IUpdateDeviceDTO {
-    readonly id: Device["id"],
-    readonly name?: Device["name"],
-    readonly sensors?: Device["sensors"]
-}
+export type TUpdateDeviceDTO = {
+    readonly id: Device["id"]
+	} & Partial<Omit<Device, "id" | "createdAt">>
 
 export default interface IUpdateDevice {
-    update: (data: IUpdateDeviceDTO) => Promise<Device>
+    update: (data: TUpdateDeviceDTO) => Promise<Device>
 }
--- src/core/use-cases/user/update.ts
+++ src/core/use-cases/user/update.ts
@@ -1,9 +1,8 @@
 import { User } from "@core/entities/user.ts"
 
-export interface IUpdateUserDTO {
+export type TUpdateUserDTO = {
     readonly id: User["id"]
-}
-
+	} & Partial<Omit<User, "id" | "registeredAt">>
 export default interface IUpdateUser {
-    update: (data: IUpdateUserDTO) => Promise<User>
+    update: (data: TUpdateUserDTO) => Promise<User>
 }
