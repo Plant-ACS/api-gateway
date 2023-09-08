ee812d58a09a6145be722bc7a4d9179d0d641acb
Bruno <brunoconcli@gmail.com>
2023-09-08 15:09:34 -0300

    feat: Implement class `CreateACS`

--- src/app/use-cases/acs/create.ts
+++ src/app/use-cases/acs/create.ts
@@ -1,1 +1,28 @@
+import { IACS } from "@entities"
+import { ICreateACS, TCreateACSDTO } from "@use-cases";
+import { IACSAlreadyExists, ISaveACSRepository } from "@app/ports/repositories/acs/mod.ts";
+import { ConflictError } from "@app/errors/conflict-error.ts";
 
+export default class CreateACS implements ICreateACS {
+	constructor(
+		readonly saveACSRepository: ISaveACSRepository,
+		readonly acsAlreadyExists: IACSAlreadyExists
+	) {}
+
+	async create(data: TCreateACSDTO): Promise<IACS> {
+		if ((await this.acsAlreadyExists.alreadyExists({
+				name: data.name,
+				espId: data.espId,
+				createdAt: data.createdAt,
+				filter: {
+					order: "ASC",
+					limit: 1,
+					page: 1
+				}
+			})) === true
+		)
+		throw new ConflictError("ACS already exists")
+
+		return await this.saveACSRepository.save(data)
+	}
+}
