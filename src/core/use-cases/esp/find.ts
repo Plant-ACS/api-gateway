eed3091a2a46b824024a3e77b59ec698d1e301e8
Bruno <brunoconcli@gmail.com>
2023-09-09 08:58:07 -0300

    feat: Add method `findSearch`

--- src/core/use-cases/esp/find.ts
+++ src/core/use-cases/esp/find.ts
@@ -1,11 +1,19 @@
 import { IESP } from "@entities"
 
 export type TFindESPByIdDTO = IESP["id"]
-export type TFilterESPDTO = {
-	order: "ASC" | "DESC"
+
+export interface IFindESPSearchDTO {
+	userId: IESP["userId"],
+	ports: IESP["ports"],
+	readonly filter: {
+		order: "ASC" | "DESC",
+		limit: number,
+		page: number
+	}
 }
 
 export interface IFindESP {
 	findById: (data: TFindESPByIdDTO) => Promise<IESP>
+	findSearch: (data: IFindESPSearchDTO) => Promise<IESP[]>
 	findAll: () => Promise<IESP[]>
 }
