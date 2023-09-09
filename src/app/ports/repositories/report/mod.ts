64ceb91dee5099e9b6d5dea01ef1cb289879dc0e
Bruno <brunoconcli@gmail.com>
2023-09-09 11:54:08 -0300

    feat: Create use cases and repositories files for Report

---
+++ src/app/ports/repositories/report/mod.ts
@@ -0,0 +1,5 @@
+export * from "./already-exists.ts"
+export * from "./save.ts"
+export * from "./find.ts"
+export * from "./delete.ts"
+export * from "./update.ts"
---
+++ src/app/use-cases/report/mod.ts
@@ -0,0 +1,4 @@
+export * from "./create.ts"
+export * from "./delete.ts"
+export * from "./update.ts"
+export * from "./find.ts"
