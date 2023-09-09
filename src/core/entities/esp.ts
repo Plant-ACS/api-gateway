6da86c068ad7ce89b3bf0318cc4e238079e49976
Bruno <brunoconcli@gmail.com>
2023-09-09 08:31:35 -0300

    update: Remove extra tabs

--- src/core/entities/esp.ts
+++ src/core/entities/esp.ts
@@ -1,8 +1,8 @@
 import { IModule } from "@entities"
 import { IUser } from "@entities"
 export interface IESP {
-    id: string,
-    userId: IUser["id"],
+	id: string,
+	userId: IUser["id"],
 	ports: Array<{
 		key: number | number[],
 		module: IModule['id']
