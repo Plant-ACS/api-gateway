84487ccf302d727c4766620b609dbe91c2a11b39
Bruno Concli <brunoconcli@gmail.com>
2023-10-04 14:41:19 -0300

    feat (continue): Add `ESP` Controllers

---
+++ src/presentation/controller/esp/create.ts
@@ -0,0 +1,28 @@
+import { ICreateESP } from "@use-cases"
+import { InternalServerError } from "@app/errors/mod.ts"
+import { IController, IRequest, IResponse, IValidator } from "@app/ports/presentation/mod.ts"
+
+export class CreateESPController implements IController {
+  constructor(
+    private readonly validationBody: IValidator,
+    private readonly createESP: ICreateESP
+  ){}
+
+  async handle(request: IRequest): Promise<IResponse> {
+    try {
+      const error = await this.validationBody.validate(request.body)
+  
+      if (error) return error
+
+      return ({
+        statusCode: 200,
+        body: await this.createESP.create(request.body)
+      })
+    }
+    catch (error) {
+      return new InternalServerError(error)
+    }
+
+
+  }
+}
---
+++ src/presentation/controller/esp/delete.ts
@@ -0,0 +1,11 @@
+// import { IController, IRequest, IResponse } from "@app/ports/presentation/mod.ts"
+
+// export class DeleteESPController implements IController {
+//   constructor(
+//     private readonly validation
+//   ){}
+
+//   async handle(request: IRequest): Promise<IResponse> {
+//     const error = this.
+//   }
+// }
