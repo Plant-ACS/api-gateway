89ccb6ab337301e78f743875b6b8841e2ebefae0
Bruno <brunoconcli@gmail.com>
2023-10-03 21:58:51 -0300

    feat (continue): Add `DeleteACSController`

---
+++ src/presentation/controller/acs/delete.ts
@@ -0,0 +1,13 @@
+// import { IDeleteACS } from "@use-cases"
+// import { IController, IRequest, IResponse, IValidator } from "@app/ports/presentation/mod.ts";
+
+// export class DeleteACSController implements IController {
+// 	constructor(
+// 		private readonly validationParameters: IValidator,
+// 		private readonly deleteACS: IDeleteACS
+// 	) {}
+
+// 	async handle(data: IRequest): Promise<IResponse> {
+
+// 	}
+// }
