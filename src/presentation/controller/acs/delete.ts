56f87d8d6f923030107be7b754f4fa97e35e8245
Bruno <brunoconcli@gmail.com>
2023-10-04 06:20:46 -0300

    feat: Add `DeleteACSController`

--- src/presentation/controller/acs/delete.ts
+++ src/presentation/controller/acs/delete.ts
@@ -1,13 +1,30 @@
-// import { IDeleteACS } from "@use-cases"
-// import { IController, IRequest, IResponse, IValidator } from "@app/ports/presentation/mod.ts";
+import { IDeleteACS } from "@use-cases"
+import { IController, IRequest, IResponse, IValidator } from "@app/ports/presentation/mod.ts";
 
-// export class DeleteACSController implements IController {
-// 	constructor(
-// 		private readonly validationParameters: IValidator,
-// 		private readonly deleteACS: IDeleteACS
-// 	) {}
+export class DeleteACSController implements IController {
+	constructor(
+		private readonly validationParameters: IValidator,
+		private readonly deleteACS: IDeleteACS
+	) {}
 
-// 	async handle(data: IRequest): Promise<IResponse> {
+	async handle(request: IRequest): Promise<IResponse> {
+		try {
+			const error = this.validationParameters.validate(request.params)
 
-// 	}
-// }
+			if (error) throw error
+
+			await this.deleteACS.delete(request.params.id)
+
+			return ({
+				statusCode: 200,
+				body: "ACS deleted successfully"
+			})
+		}
+		catch(_error) {
+			return ({
+				statusCode: 400,
+				body: "ACS could not be deleted"
+			})
+		}
+	}
+}
