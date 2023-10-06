3c0119b3974e0286740a9b71fa728e8456d608fa
miguel-l-b <miguellopesbraido@gmail.com>
2023-10-06 16:17:41 -0300

    ref: making it easier to use the provider

--- src/app/ports/providers/restful.ts
+++ src/app/ports/providers/restful.ts
@@ -1,16 +1,28 @@
+import { IResponse } from "@app/ports/presentation/response.ts";
+import { IRequest } from "@app/ports/presentation/request.ts";
+
+export interface ILogger {
+	info: (message: string) => void,
+	error: (message: string) => void,
+	warn: (message: string) => void,
+}
+
+export type IRequestCallback = (req: IRequest) => IResponse
+export type IMiddlewareCallback = (req: IRequest, next: () => void) => IResponse
+
 export interface IRouter {
-	use(path: string, callback: (req: any, res: any, next: any) => void | IRouter): void
-	get(path: string, callback: (req: any, res: any) => void): void
-	post(path: string, callback: (req: any, res: any) => void): void
-	put(path: string, callback: (req: any, res: any) => void): void
-	delete(path: string, callback: (req: any, res: any) => void): void
+	use(path: string, callback: (IMiddlewareCallback | IRouter)): void,
+	get(path: string, callback: IRequestCallback): void,
+	post(path: string, callback: IRequestCallback): void,
+	put(path: string, callback: IRequestCallback): void,
+	delete(path: string, callback: IRequestCallback): void
 }
 
 export interface IRestFul {
 	listen(callback?: (host: string, port: number) => void): void
-	use(path?: string, callback?: (req: any, res: any, next: any) => void | IRouter): void
-	get(path: string, callback: (req: any, res: any) => void): void
-	post(path: string, callback: (req: any, res: any) => void): void
-	put(path: string, callback: (req: any, res: any) => void): void
-	delete(path: string, callback: (req: any, res: any) => void): void
+	use(path: string, callback: (IMiddlewareCallback | IRouter)): void,
+	get(path: string, callback: IRequestCallback): void,
+	post(path: string, callback: IRequestCallback): void,
+	put(path: string, callback: IRequestCallback): void,
+	delete(path: string, callback: IRequestCallback): void
 }
