efd1d291e7ba89a40f3a87f36c0594d12029f1a9
Bruno <brunoconcli@gmail.com>
2023-10-08 10:30:15 -0300

    feat: Organize repository in a single type

--- src/app/ports/repositories/acs/mod.ts
+++ src/app/ports/repositories/acs/mod.ts
@@ -1,5 +1,21 @@
-export * from "./already-exists.ts"
-export * from "./save.ts"
-export * from "./delete.ts"
-export * from "./find.ts"
-export * from "./update.ts"
+import { type IACSAlreadyExists } from "./already-exists.ts"
+import { type ISaveACSRepository } from "./save.ts"
+import { type IDeleteACSRepository } from "./delete.ts"
+import { type IFindACSRepository } from "./find.ts"
+import { type IUpdateACSRepository } from "./update.ts"
+
+type IACSRepository =
+	IACSAlreadyExists &
+	ISaveACSRepository &
+	IDeleteACSRepository &
+	IFindACSRepository &
+	IUpdateACSRepository
+
+export type {
+	IACSRepository,
+	IACSAlreadyExists,
+	ISaveACSRepository,
+	IDeleteACSRepository,
+	IFindACSRepository,
+	IUpdateACSRepository,
+}
