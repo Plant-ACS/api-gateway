4d8ca7d02d7c9af70bb34b797bd25b9fd7ed7360
Bruno <brunoconcli@gmail.com>
2023-10-08 19:12:32 -0300

    ref: Change mod to fit `IESPRepository`

--- src/app/ports/repositories/esp/mod.ts
+++ src/app/ports/repositories/esp/mod.ts
@@ -1,5 +1,27 @@
+import { IESPAlreadyExistsRepository } from "@app/ports/repositories/esp/already-exists.ts"
+import { ISaveESPRepository } from "@app/ports/repositories/esp/save.ts"
+import { IDeleteESPRepository } from "@app/ports/repositories/esp/delete.ts"
+import { IFindESPRepository } from "@app/ports/repositories/esp/find.ts"
+import { IUpdateESPRepository } from "@app/ports/repositories/esp/update.ts"
+
 export * from "./already-exists.ts"
 export * from "./save.ts"
 export * from "./delete.ts"
 export * from "./find.ts"
 export * from "./update.ts"
+
+type IESPRepository =
+	IESPAlreadyExistsRepository &
+	ISaveESPRepository &
+	IDeleteESPRepository &
+	IFindESPRepository &
+	IUpdateESPRepository
+
+export type {
+	IESPRepository,
+	IESPAlreadyExistsRepository,
+	ISaveESPRepository,
+	IDeleteESPRepository,
+	IFindESPRepository,
+	IUpdateESPRepository
+}
