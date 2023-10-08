ebc42f6f440088f4b15b2d1c6ef5147c8d0dc405
Bruno <brunoconcli@gmail.com>
2023-10-08 18:32:16 -0300

    ref: Small change

--- src/infrastructure/providers/mongodb-connection.ts
+++ src/infrastructure/providers/mongodb-connection.ts
@@ -1,16 +1,14 @@
 import mongoose from 'mongoose'
 let connection: typeof mongoose|null = null
 
-async function startConnection() {
+export async function startConnection() {
 	connection = await mongoose.connect("mongodb+srv://root-solis:E17FCsvnyh7oH4jM@solis.imfvv3c.mongodb.net/?retryWrites=true&w=majority")
 }
 
-async function getConnectionDB() {
+export default async function getConnectionDB() {
 	if (connection == null)
 		await startConnection()
 	if(connection != null)
 		return connection
 	throw new Error('Connection not started')
 }
-
-export default getConnectionDB
