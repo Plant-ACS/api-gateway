656864786530be4a2f93ef38852256d3c53c12f4
Bruno <brunoconcli@gmail.com>
2023-10-09 17:02:02 -0300

    feat: Conclude `ESPSchema`

--- src/infrastructure/repositories/mongodb/models/esp.ts
+++ src/infrastructure/repositories/mongodb/models/esp.ts
@@ -6,6 +6,15 @@ const ESPSchema = new Schema({
 	userId: {
 		type: String,
 		required: true
+	},
+	ports: {
+		type: Array<{
+			key: Number | Number[],
+			module: String
+		}>,
+		required: true
 	}
-
 })
+
+const ESPDB = model("IESP", ESPSchema)
+export default ESPDB
