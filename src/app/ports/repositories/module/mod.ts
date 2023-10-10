c92b8026f6d999426a41e5f94ba449cfdb324775
Bruno <brunoconcli@gmail.com>
2023-10-09 21:49:10 -0300

    ref: Update mod to support new type

--- src/app/ports/repositories/module/mod.ts
+++ src/app/ports/repositories/module/mod.ts
@@ -1,5 +1,21 @@
+import { IModuleAlreadyExistsRepository } from "@app/ports/repositories/module/already-exists.ts"
+import { ISaveModuleRepository } from "@app/ports/repositories/module/save.ts"
+import { IDeleteModuleRepository } from "@app/ports/repositories/module/delete.ts"
+import { IFindModuleRepository } from "@app/ports/repositories/module/find.ts"
+import { IUpdateModuleRepository } from "@app/ports/repositories/module/update.ts"
+
 export * from "./save.ts"
 export * from "./already-exists.ts"
 export * from "./delete.ts"
 export * from "./find.ts"
 export * from "./update.ts"
+
+type IModuleRepository =
+	IModuleAlreadyExistsRepository &
+	ISaveModuleRepository &
+	IDeleteModuleRepository &
+	IFindModuleRepository &
+	IUpdateModuleRepository
+
+export type { IModuleRepository }
+
