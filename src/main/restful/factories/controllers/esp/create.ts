9700e8f015ef2e0b77802eca04d733b4f5eb7020
Bruno Concli <brunoconcli@gmail.com>
2023-10-16 16:38:59 -0300

    feat (to continue): Start facotries

---
+++ src/main/restful/factories/controllers/esp/create.ts
@@ -0,0 +1,16 @@
+import { CreateESP } from "@app/use-cases/esp/create.ts";
+import { CreateESPController } from "@pre/controller/mod.ts"
+import { CompositeValidation } from "@pre/validation/composite.ts";
+import { RequiredValidator, TypeFieldValidator} from "@pre/validation/mod.ts";
+import { StringType } from "@pre/validation/types/string.ts";
+
+export const CreateESPControllerFactory = (): CreateESPController => {
+  return new CreateESPController(
+    new CompositeValidation([
+      new RequiredValidator("userId"),
+      new RequiredValidator("ports"),
+      new TypeFieldValidator("userId", [new StringType()]),
+    ]),
+    new CreateESP(/*... falta os repositórios ...*/))
+
+}
--- src/main/restful/routes/esp.ts
+++ src/main/restful/routes/esp.ts
@@ -1,21 +1,12 @@
 import { express } from "@infra/providers/restful/mod.ts"
 import { adaptController } from "@main/restful/adapters/adaptController.ts"
 import { CreateESPController } from "@pre/controller/esp/mod.ts"
-import { CreateESP } from "@app/use-cases/esp/mod.ts"
-import { CompositeValidation, RequiredValidator, TypeFieldValidator } from "@pre/validation/mod.ts"
-import { StringType } from "@pre/validation/types/mod.ts";
+
 
 const routerEsp = new express.Router()
 
 routerEsp.post("/",
-	adaptController(
-		new CreateESPController(
-			new CompositeValidation([
-				new RequiredValidator("userId"),
-				new RequiredValidator("ports"),
-				new TypeFieldValidator("userId", [new StringType()]),
-			]),
-			new CreateESP(/*... falta os repositórios ...*/)))
+	adaptController()
 )
 
 export default routerEsp
