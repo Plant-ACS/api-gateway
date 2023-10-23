4a0b6571b0d377d14fd9e4e2ea613f3aa2b25c27
Bruno <brunoconcli@gmail.com>
2023-10-23 05:34:01 -0300

    feat: add then and catch to adaptController

--- src/main/restful/adapters/adaptController.ts
+++ src/main/restful/adapters/adaptController.ts
@@ -2,4 +2,6 @@ import { IController, IRequest, IResponse } from "@app/ports/presentation/mod.ts
 
 export function adaptController(controller: IController) {
 	return async (req: IRequest): Promise<IResponse> => await controller.handle(req)
+		.then((response: IResponse) => ({ statusCode: response.statusCode, body: response.body }))
+		.catch((error: Error) => ({ statusCode: 500, body: error.message }))
 }
