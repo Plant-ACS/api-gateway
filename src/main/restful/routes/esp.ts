491980a81fe8219b123ef44c8e72dd160cc0eb9d
Bruno <brunoconcli@gmail.com>
2023-10-23 06:03:52 -0300

    feat (to continue & review): Implementing `ESP` routes

--- src/main/restful/routes/esp.ts
+++ src/main/restful/routes/esp.ts
@@ -1,12 +1,32 @@
 import { express } from "@infra/providers/restful/mod.ts"
 import { adaptController } from "@main/restful/adapters/adaptController.ts"
-import { CreateESPController } from "@pre/controller/esp/mod.ts"
-
+import {
+	CreateESPControllerFactory,
+	FindAllESPControllerFactory,
+	FindESPByIdControllerFactory,
+	FindESPSearchControllerFactory,
+	DeleteESPControllerFactory,
+	UpdateESPControllerFactory
+} from "../factories/controllers/mod.ts"
 
 const routerEsp = new express.Router()
 
+routerEsp.get("/",
+	adaptController(FindAllESPControllerFactory())
+)
+
+routerEsp.get("/:id",
+	adaptController(FindESPByIdControllerFactory())
+)
+
+routerEsp.get("/:id", adaptController(FindESPSearchControllerFactory())) // review
+
 routerEsp.post("/",
-	adaptController()
+	adaptController(CreateESPControllerFactory())
+)
+
+routerEsp.delete("/:id",
+	adaptController(DeleteESPControllerFactory())
 )
 
 export default routerEsp
